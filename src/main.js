cede73f1ad34336ee2ee09e46a113a35836af01c
Samuel Gonzalez <sagonzalez@aidacanarias.com>
2024-11-28 21:44:34 +0000

    feat: ✨ Added management of notification permission after installing the service worker

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const publicVapidKey = import.meta.env.VITE_PUBLIC_VAPID_KEY;
 const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
 if ("serviceWorker" in navigator) {
-  send().catch((err) => console.error(err));
+  registerWorker().catch((err) => console.error(err));
 }
 
 document.getElementById("notifyButton").addEventListener("click", () => {
@@ -48,24 +48,73 @@ document.getElementById("unsubscribeButton").addEventListener("click", async () 
   }
 });
 
-async function send() {
-  const register = await navigator.serviceWorker.register("/sw.js", {
-    scope: "/",
-  });
+async function registerWorker() {
+  try {
+    await navigator.serviceWorker.register("/sw.js", {
+      scope: "/",
+    });
 
-  const subscription = await register.pushManager.subscribe({
-    userVisibleOnly: true,
-    applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-  });
+    if(!navigator.serviceWorker.controller) {
+      console.log('Service Worker aún no controla la página.');
+      await new Promise(resolve => {
+        navigator.serviceWorker.addEventListener('controllerchange', () => {
+          console.log('Service Worker ahora controla la página.');
+          resolve();
+        });
+      });
+      await requestNotificationPermission()
+    } else {
+      await requestNotificationPermission()
+    }
 
-  await fetch(`${backendUrl}/subscribe`, {
-    method: "POST",
-    body: JSON.stringify(subscription),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+    await registerSubscription()
+
+  } catch (error) {
+    console.error("Error al registrar el service worker:", error);
+  }
+
 }
+
+async function requestNotificationPermission() {
+  const LOCAL_STORAGE_ID = 'notifications_requested';
+
+  const isGranted = localStorage.getItem(LOCAL_STORAGE_ID);
+  if (isGranted === 'true') {
+    return;
+  }
+
+  if (Notification.permission === 'granted' || Notification.permission === 'denied') {
+    return;
+  }
+
+  const permission = await Notification.requestPermission()
+  if (permission === 'granted') {
+    console.log('Notificaciones permitidas.');
+  } else {
+    console.log('Permiso de notificaciones rechazado.');
+  }
+  localStorage.setItem(LOCAL_STORAGE_ID, 'true');
+}
+
+const registerSubscription = async () => {
+  try {
+    const registration = await navigator.serviceWorker.ready;
+    const subscription = await registration.pushManager.subscribe({
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+    });
+
+    await fetch(`${backendUrl}/subscribe`, {
+      method: "POST",
+      body: JSON.stringify(subscription),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (error) {
+    console.error("Error al suscribir al usuario:", error);
+  }
+};
 
 function urlBase64ToUint8Array(base64String) {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
