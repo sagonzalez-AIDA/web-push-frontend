e79c32fc336cecf1c5e345f5a6dba52e88d83194
Samuel Gonzalez <sagonzalez@aidacanarias.com>
2024-11-28 21:41:24 +0000

    feat: ✨ added that the service worker takes control as soon as it is installed

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,11 @@
+self.addEventListener("install", () => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(clients.claim());
+})
+
 self.addEventListener("push", (event) => {
   event.waitUntil(
       clients.matchAll({ type: "window", includeUncontrolled: false }).then((clientList) => {
